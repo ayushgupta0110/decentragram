b90ea08c11d44e37cee4639c5081cef21115b873
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-08-14 19:01:25 +0530

    Completed adding basic contract

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,34 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
   string public name = "Decentragram";
+
+  //Store Images
+  uint public imageCount = 0;
+  mapping (uint => Image) public images;
+  
+  struct Image{
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;  
+  }
+  
+  event imageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author 
+  );
+
+  //Create Images
+    function uploadImage(string memory _imagehash,string memory _description) public{
+      imageCount++;
+      images[imageCount] = Image(imageCount,_imagehash,_description, 0, msg.sender);
+    //Emit the event 
+      emit imageCreated(imageCount,_imagehash,_description,0,msg.sender);
+    }
+
+  //Tip Images
 }
